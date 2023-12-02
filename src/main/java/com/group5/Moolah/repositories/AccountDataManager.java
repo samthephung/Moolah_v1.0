1079ac30daafff182b900471d3c313fa8c5e47f7
samthephung <sphungs03@gmail.com>
2023-12-02 18:43:18 -0500

    updated structure to include service classes and repository classes

---
+++ src/main/java/com/group5/Moolah/repositories/AccountDataManager.java
@@ -0,0 +1,6 @@
+package com.group5.Moolah.repositories;
+
+public class AccountDataManager {
+
+    //this is where the actual CRUD operations will be performed -- reaching out directly to the MongoDB from this class
+}
---
+++ src/main/java/com/group5/Moolah/services/AccountService.java
@@ -0,0 +1,7 @@
+package com.group5.Moolah.services;
+
+public class AccountService {
+
+    //acts as an abtraction between the controller and repository classes -- since the controller should only speak to the service layer (which are these classes)
+    //these classes will call the repository methods that perform the create, update, retrieve and delete operations
+}
