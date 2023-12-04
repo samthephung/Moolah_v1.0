e5c529c94dec8003ac464e3f0eab26dc017dd404
fletcherh <fqh1215@gmail.com>
2023-12-04 18:12:27 -0500

    calculation test updates

--- src/test/java/com/group5/Moolah/ExpenseCalculationTest.java
+++ src/test/java/com/group5/Moolah/ExpenseCalculationTest.java
@@ -12,7 +12,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ExpenseCalculationTest {
-    /*
+
     //create a new user and prepopulate expenses for each test
     @BeforeClass
     public static void populateWithExpenses(){
@@ -20,36 +20,35 @@ public class ExpenseCalculationTest {
         ExpenseDataManager exp = new ExpenseDataManager(client);
 
         //date -- October 1st 2023 (10/1/23) -- October Dates
-        List<Integer> date1 = new ArrayList<>(){ {add(1); add(10); add(2023);} };
-        List<Integer> date2 = new ArrayList<>(){ {add(12); add(10); add(2023);} };
-        List<Integer> date3 = new ArrayList<>(){ {add(17); add(10); add(2023);} };
-
+        String date1 = "2023-10-01";
+        String date2 = "2023-10-12";
+        String date3 = "2023-10-17";
         //date -- February 24th 2023 (2/24/23) -- February Dates
-        List<Integer> date4 = new ArrayList<>(){ {add(24); add(2); add(2023);} };
+        String date4 = "2023-02-24";
         //date -- August 23rd 2023 (8/23/2023) -- August Dates
-        List<Integer> date5 = new ArrayList<>(){ {add(23); add(8); add(2023);} };
+        String date5 = "2023-08-23";
 
 
         //one user -- identified by their email -- sp@gmail.com
-        Expense e = new Expense(date1, 25.00, "Take-out", "cash", false);
+        Expense e = new Expense("Take-out", 25.00, date1, "cash", "one-time", "food");
         exp.addExpense("sp@gmail.com", e);
 
-        e = new Expense(date2, 5.25, "Uber", "card", false);
+        e = new Expense("Uber", 5.25, date2, "card", "weekly", "travel");
         exp.addExpense("sp@gmail.com", e);
 
-        e = new Expense(date3, 3.05, "Chips", "cash", false);
+        e = new Expense("Chips", 3.05, date3, "cash", "one-time", "food");
         exp.addExpense("sp@gmail.com", e);
 
-        e = new Expense(date4, 125.12, "Groceries for the Week", "card", false);
+        e = new Expense("Groceries for the Week", 125.12, date4, "card", "weekly", "groceries");
         exp.addExpense("sp@gmail.com", e);
 
-        e = new Expense(date4, 45.28, "Books", "card", false);
+        e = new Expense("Books", 45.28, date4, "card", "monthly", "fun");
         exp.addExpense("sp@gmail.com", e);
 
-        e = new Expense(date4, 44.02, "Top Golf", "card", false);
+        e = new Expense("Top Golf", 44.02, date4, "card", "one-time", "fun");
         exp.addExpense("sp@gmail.com", e);
 
-        e = new Expense(date5, 35.00, "Cake", "cash", false);
+        e = new Expense("Cake", 35.00, date5, "cash", "yearly", "food");
         exp.addExpense("sp@gmail.com", e);
     }
 
@@ -66,7 +65,8 @@ public class ExpenseCalculationTest {
     public void getDayTotal_SingleExpense_Success(){
         MongoClient client = MongoClients.create(Constants.URI);
         CalculationManager ec = new CalculationManager(client);
-        List<Integer> date = new ArrayList<>(){ {add(1); add(10); add(2023);} };
+        String date = "2023-10-01";
+
 
         assertEquals(25.00, ec.getDayTotal("sp@gmail.com", date), 0.01);
     }
@@ -77,7 +77,7 @@ public class ExpenseCalculationTest {
 
         MongoClient client = MongoClients.create(Constants.URI);
         CalculationManager ec = new CalculationManager(client);
-        List<Integer> date = new ArrayList<>(){ {add(24); add(2); add(2023);} };
+        String date = "2023-02-24";
 
         assertEquals(214.42, ec.getDayTotal("sp@gmail.com", date), 0.01);
 
@@ -89,18 +89,19 @@ public class ExpenseCalculationTest {
 
         MongoClient client = MongoClients.create(Constants.URI);
         CalculationManager ec = new CalculationManager(client);
-        List<Integer> date = new ArrayList<>(){ {add(5); add(2); add(2023);} };
+        String date = "2020-12-12";
 
         assertEquals(0, ec.getDayTotal("sp@gmail.com", date), 0.01);
 
     }
 
     //get total of all expenses from a specific month and user -- handle a single expense
+    /*
     @Test(timeout = 5000)
     public void getMonthTotal_SingleExpense_Success(){
         MongoClient client = MongoClients.create(Constants.URI);
         CalculationManager ec = new CalculationManager(client);
-        List<Integer> date = new ArrayList<>(){ {add(23); add(8); add(2023);} };
+        String date = "2023-08-23";
 
         assertEquals(35.00, ec.getMonthTotal("sp@gmail.com", date), 0.01);
 
@@ -111,7 +112,7 @@ public class ExpenseCalculationTest {
     public void getMonthTotal_MultipleExpenses_Success(){
         MongoClient client = MongoClients.create(Constants.URI);
         CalculationManager ec = new CalculationManager(client);
-        List<Integer> date = new ArrayList<>(){ {add(28); add(10); add(2023);} };
+        String date = "2023-10-28";
 
         assertEquals(33.30, ec.getMonthTotal("sp@gmail.com", date),  0.01);
     }
@@ -121,11 +122,13 @@ public class ExpenseCalculationTest {
     public void getMonthTotal_NoExpenses_Success(){
         MongoClient client = MongoClients.create(Constants.URI);
         CalculationManager ec = new CalculationManager(client);
-        List<Integer> date = new ArrayList<>(){ {add(14); add(5); add(2023);} };
+        String date = "2023-05-14";
 
         assertEquals(0, ec.getMonthTotal("sp@gmail.com", date), 0.01);
 
     }
-    */
+     */
+
+
 
 }
